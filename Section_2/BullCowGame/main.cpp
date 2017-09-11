456a847f559e01465df69f0cd9e441a660544c19
flizzer <briandavis1977@gmail.com>
2017-09-10 22:12:23 -0400

    further work on Bulls and Cows

--- Section_2/BullCowGame/main.cpp
+++ Section_2/BullCowGame/main.cpp
@@ -1,4 +1,24 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
 int main()
 {
+	constexpr int WORD_LENGTH = 5;
+
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram ";
+	cout << "I'm thinking of?\n";
+
+	cout << "Enter your guess: ";
+	string Guess = "";
+	cin >> Guess;
+	cout << "\nYou guessed: " << Guess << endl;
+	
+	cout << "Enter your guess: ";
+	cin >> Guess;
+	cout << "\nYou guessed: " << Guess << endl;
+
 	return 0;
 }
