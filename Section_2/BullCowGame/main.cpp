9948612885e70581a5ef4415ffa1abc1df33e8e2
flizzer <briandavis1977@gmail.com>
2017-09-12 22:39:15 -0400

    Refactoring to start to use methods

--- Section_2/BullCowGame/main.cpp
+++ Section_2/BullCowGame/main.cpp
@@ -3,23 +3,35 @@
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrintBack();
+
+
 int main()
+{
+	PrintIntro();
+	GetGuessAndPrintBack();
+	GetGuessAndPrintBack();
+	cout << endl;
+	return 0;
+}
+
+void PrintIntro()
 {
 	constexpr int WORD_LENGTH = 5;
 
 	cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	cout << "Can you guess the " << WORD_LENGTH << " letter isogram ";
 	cout << "I'm thinking of?\n";
+	cout << endl;
+	return;
+}
 
+string GetGuessAndPrintBack()
+{
+	cout << "Enter your guess: ";
 	string Guess = "";
-
-	cout << "Enter your guess: ";
-	getline(cin,Guess);
-	cout << "\nYou guessed: " << Guess << endl;
-
-	cout << "Enter your guess: ";
 	getline(cin, Guess);
-	cout << "\nYou guessed: " << Guess << endl;
-
-	return 0;
+	cout << "You guessed: " << Guess << endl;
+	return Guess;
 }
